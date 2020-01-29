00e792ce9d5c99a2a4ee6b718d58e8fdf8bfe295
114Deekay <60127393+114Deekay@users.noreply.github.com>
2020-01-29 16:15:18 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string DepartmentName { get; set; }
+
+        public Employee(string id, string name, string deptName)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.DepartmentName = deptName;
+        
+        }
+
+        public string GetId()
+        {
+            FunctionCall();
+            return Id;
+        }
+
+        public string GetName()
+        {
+            FunctionCall();
+            return Name;
+        }
+
+        public string GetDeparmentName()
+        {
+            FunctionCall();
+            return DepartmentName;
+        }
+        public void update(string )
+        {
+            
+        }
+
+        public event FunctionCallDelegate FunctionCall;
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter th Id, Name and Department of the Employee, respectively");
+            string id = Console.ReadLine();
+            string name = Console.ReadLine();
+            string deptName = Console.ReadLine();
+
+            Employee emp = new Employee(id, name, deptName);
+            emp.FunctionCall += new FunctionCallDelegate(OnFunctionCall);
+            emp.FunctionCall += new FunctionCallDelegate(OnFunctionCall2);
+            emp.FunctionCall += new FunctionCallDelegate(OnFunctionCall3);
+
+            Console.WriteLine("Employee Id = "+emp.GetId());
+            Console.WriteLine("Employee Name = " + emp.GetName());
+            Console.WriteLine("Employee Department = " + emp.GetDeparmentName());
+            Console.ReadKey();
+            
+           
+        }
+        public static string OnFunctionCall()
+        {
+            return "GetId() Called";
+        }
+
+        public static string OnFunctionCall2()
+        {
+            return "GetName() Called";
+        }
+
+        public static string OnFunctionCall3()
+        {
+            return "GetDepartmentName() Called";
+        }
+
+    }
+}
